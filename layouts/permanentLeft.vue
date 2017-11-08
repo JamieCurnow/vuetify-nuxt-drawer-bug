b255b4215d6322f17b30201e7b9618007e7edfb7
JamieCurnow <jamie@island-web.ca>
2017-11-08 13:02:05 -0800

    init commit bug.

---
+++ layouts/permanentLeft.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      app
+      enable-resize-watcher
+      disable-route-watcher
+      persistent
+      v-model="drawer"
+      light
+      >
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.title" :to="item.html">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <main>
+      <v-content fluid class="pt-0">
+        <v-container fluid style="padding:0px;">
+          <nuxt />
+        </v-container>
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: true,
+      snackText: '',
+      snackOn: false,
+      menuItems: [
+        {
+          title: 'Temp Right',
+          icon: 'bug_report',
+          html: '/temporary-right'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/temporaryRight.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      app
+      temporary
+      v-model="drawer"
+      light
+      right
+    >
+      <v-flex text-xs-center class="pl-5">
+        <v-list>
+          <v-list-tile nuxt :to="item.html" v-for="item in mobileMenuItems" :key="item.title">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-navigation-drawer>
+
+    <v-toolbar fixed class="white elevation-1" app>
+      <v-spacer></v-spacer>
+      <v-btn icon light @click.stop.native="drawer = true" class="">
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <main>
+      <v-content fluid>
+        <v-container fluid>
+          <nuxt />
+        </v-container>
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      mobileMenuItems: [
+        {title: 'Home', icon: 'home', html: '/'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
